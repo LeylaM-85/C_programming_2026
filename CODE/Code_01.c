cf4571022ef55e05e1ea8023e681b848f7efb5cb
LeylaM. <leyla.memmedova.eldar@gmail.com>
2026-03-11 06:20:39 +0400

    Create Code_01.c

---
+++ CODE/Code_01.c
@@ -0,0 +1,12 @@
+/*    5
+    125
+   1024
+bu formada çap olunsun ededler (%5d)
+      */
+
+#include <stdio.h>
+int main(){
+  int a,b,c;
+  a=5,b=28, c=386;
+  printf("%5d\n %5d\n %6d",a,b,c );
+}
